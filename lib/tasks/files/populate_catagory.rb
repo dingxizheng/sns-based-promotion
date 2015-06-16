07354c36c3cd5b34a3790877993fa2d8f1de4bf6
dingxizheng <dingxizheng@gmail.com>
2015-06-16 02:26:59 -0400

    bug fixing

--- lib/tasks/files/populate_catagory.rb
+++ lib/tasks/files/populate_catagory.rb
@@ -16,18 +16,27 @@ catagories.each do |c|
   # puts c
   filename = c[:icon]
 
-  if not Catagory.where(c.except!(:icon)).first.present?
+  catagory = Catagory.where(c.except!(:icon)).first
+
+  if not catagory.present?
 
   	puts 'import catagory:' + c[:name]
 
     cgr = Catagory.new(c.except!(:icon))
+    cgr.save
 
     icon = Image.new
     icon.store(nil, File.read(filename))
     icon.save
-    cgr.icon = icon
-    cgr.save
 
+    cgr.icon = icon
+
+  elsif catagory.present? and not catagory.icon.present?
+  	icon = Image.new
+    icon.store(nil, File.read(filename))
+    icon.save
+
+  	catagory.icon = icon
   end
 
 end
