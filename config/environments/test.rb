122c7a95c190d3bf629be0c8f4eb1339a91c30e9
dingxizheng <dingxizheng@gmail.com>
2015-07-23 22:21:06 -0400

    small change

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,9 +37,9 @@ GampApi::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'ENV["GMAIL_USERNAME"]',
-    user_name:            'ENV["GMAIL_USERNAME"]',
-    password:             'ENV["GMAIL_PASSWORD"]',
+    domain:               ENV["GMAIL_USERNAME"],
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto:  true  
   }
