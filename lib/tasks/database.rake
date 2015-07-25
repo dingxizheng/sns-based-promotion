a1cf28cb9ac07ee12753cd738ba32cc3f69a2680
dingxizheng <dingxizheng@gmail.com>
2015-07-24 22:48:09 -0400

    config changes

--- lib/tasks/database.rake
+++ lib/tasks/database.rake
@@ -1,15 +1,17 @@
 namespace :database do
   
   desc "populate catagories"
-  task catagory: :environment do
+  task :populate_catagory => :environment do
+  	Mongoid.load!("config/mongoid.yml", :test)
   	load 'lib/tasks/files/populate_catagory.rb' 
   end
 
   desc "add admin user"
-  task add_admin_user: :environment  do
-  	mongoid_yml = File.join(Rails.root, "config/mongoid.yml")
-    Mongoid.load! mongoid_yml, :test
-  	puts User.all.count
+  task :add_admin_user => :environment  do
+  	Mongoid.load!("config/mongoid.yml", :test)
+  	User.reindex
+  	Sunspot.commit
+	# puts User.all.count
   	# load 'lib/tasks/files/add_admin_user.rb'
   end
 
--- lib/tasks/files/add_admin_user.rb
+++ lib/tasks/files/add_admin_user.rb
@@ -1,20 +1,21 @@
 
-puts ENV['MONGO_USERNAME']
+# puts ENV['MONGO_USERNAME']
 
-# user1 = {
-#   name: 'dingxizheng',
-#   email: 'dingxizheng@gmail.com',
-#   password: 'admin',
-#   phone: '807-631-9942',
-#   address: '472 Rupert Str. Thunder Bay, Ontario',
-#   description: 'The data can then be loaded with the rake db:seed',
-#   keywords: ['admin', 'user']
-# }
 
-# if not User.where({ :email => user1[:email] }).first.present?
+user1 = {
+  name: 'dingxizheng',
+  email: 'dingxizheng@gmail.com',
+  password: 'admin',
+  phone: '807-631-9942',
+  address: '472 Rupert Str. Thunder Bay, Ontario',
+  description: 'The data can then be loaded with the rake db:seed',
+  keywords: ['admin']
+}
 
-#   user = User.new(user1)
-#   user.add_role :admin
-#   user.save
+if not User.where({ :email => user1[:email] }).first.present?
 
-# end
+  user = User.new(user1)
+  user.add_role :admin
+  user.save
+
+end
