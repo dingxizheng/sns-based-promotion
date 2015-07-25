d66460f0171ac710213d4214cb8c723acdf31ed7
dingxizheng <dingxizheng@gmail.com>
2015-07-25 02:36:22 -0400

    with default host set

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,13 +26,15 @@ GampApi::Application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  config.action_controller.default_url_options = { host: 'rails-api-env-b4cm2bfxbr.elasticbeanstalk.com' }
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'rails-api-env-b4cm2bfxbr.elasticbeanstalk.com' }
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
