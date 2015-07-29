cb101f34dbc8712070af6cb63a6ee90414ead050
dingxizheng <dingxizheng@gmail.com>
2015-07-29 15:24:50 -0400

    teepan as admin addedd

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: "donotreply@example.com"
 
   def admin_users
-  	['dingxizheng@gmail.com']
-  end 
+  	['dingxizheng@gmail.com', 'teepan.nanthakumar@gmail.com']
+  end
 
 end
