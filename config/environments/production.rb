b5187ca758c18535cd338eb1c1e4472691e60f7a
dingxizheng <dingxizheng@gmail.com>
2015-08-17 12:14:39 -0400

    bug fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ GampApi::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.config.serve_static_file = false
+  config.serve_static_file = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
