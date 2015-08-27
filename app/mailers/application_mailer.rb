772daa4ae56382faa6fbd3648bfdc6c2ecfdb7e5
dingxizheng <dingxizheng@gmail.com>
2015-08-27 18:24:22 -0400

    new email templates

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@vicinity.deals'
+  default from: 'Vicinity Deals <info@vicinity.deals>'
 
   def admin_users
   	ENV['ADMIN_USERS'].split(',')
